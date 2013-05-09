1154b78676d75b3df57dcc980b3c360d60bd0d18
Andrew <forkk@forkk.net>
2013-05-09 17:03:45 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+MCRewind
+========
+
+MCRewind is an open-source project whose goal is to replace the now-defunct MCNostalgia.
+
+It consists of a set of scripts for generating patches to downgrade the latest version of Minecraft to older versions. In addition to generating these patches, it also generates an 'index' file containing information about the generated patches. The index and patches can then be hosted on a webserver, where clients can read the index and download / use the patches.
+
+
+
+License
+=======
+
+MCRewind is licensed under the MIT license.
+
+Copyright (C) 2013 Andrew Okin
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
---
+++ patchgen.py
@@ -0,0 +1,215 @@
+# Copyright (C) 2013 Andrew Okin
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+import json
+import urllib2
+import md5
+import re
+
+from urlparse import urljoin
+
+import os
+import os.path
+import platform
+import sys
+
+from subprocess import call
+
+import fsutils
+
+
+
+# URL for Mojang's version list.
+version_list_url = "http://s3.amazonaws.com/Minecraft.Download/versions/"
+
+# Path to the temporary folder.
+temp_dir_path = "tmp"
+
+# Path to the jars folder.
+jar_dir_path = "jars"
+
+# Path to the output folder.
+output_dir_path = "output"
+
+
+# Determine the path to bsdiff
+bsdiff_path = ""
+if os.name == "nt":
+	# On Windows, bsdiff is at bsdiff/bsdiff.exe
+	bsdiff_path = "bsdiff/bsdiff.exe"
+
+elif os.name == "posix":
+	# In Posix systems, we'll try to find bsdiff installed on the system somewhere.
+	print "Trying to find bsdiff..."
+	result = ""
+	if not call("which bsdiff", result):
+		print "Found bsd`iff at " + result
+		bsdiff_path = result
+	else:
+		print "Could not find bsdiff. Aborting."
+		exit(1)
+
+else:
+	print "Your operating system is not supported. Aborting."
+	exit(1)
+
+print "bsdiff path is " + bsdiff_path
+
+
+
+def determine_latest_version():
+	"""Reads from Mojang's version list and returns the name of the latest version of Minecraft."""
+
+	response = urllib2.urlopen(urljoin(version_list_url, "versions.json"))
+	json_str = response.read()
+
+	data = json.loads(json_str)
+
+	return data["latest"]["release"]
+
+
+def download_version(mcversion, dest, max_tries = 3):
+	"""Downloads the jar file with the given version number from Mojang's download site to the given output file."""
+	try_count = 0
+	
+	while try_count < max_tries:
+		response = urllib2.urlopen(urljoin(version_list_url, mcversion + "/" + mcversion + ".jar"))
+		outfile = open(dest, "wb")
+		info = response.info()
+
+		etag = (info.getheaders("ETag")[0])[1:-1]
+		size = int(info.getheaders("Content-Length")[0])
+		print "Downloading %s bytes" % size
+
+		# Ensure that the etag is a valid MD5
+		if len(etag) != 32 or not re.findall(r"[a-fA-F\d]", etag):
+			print "ETag " + etag + " is not a valid MD5. Aborting!"
+			exit(1)
+
+		m = md5.new()
+
+		downloaded = 0
+		block_sz = 8192
+		while True:
+			buf = response.read(block_sz)
+			if not buf:
+				break
+
+			downloaded += len(buf)
+			outfile.write(buf)
+			m.update(buf)
+
+			pcnt = int((float(downloaded) / float(size)) * 100)
+			sys.stdout.write("\rDownloading jar file - %3d%% [%7d/%7d bytes]" % (pcnt, downloaded, size))
+			sys.stdout.flush()
+
+		sys.stdout.write("\n")
+		sys.stdout.flush()
+
+		outfile.close()
+
+		digest = m.hexdigest()
+		if digest == etag:
+			print "Downloaded successfully"
+			break
+		elif try_count < max_tries:
+			print "MD5 of downloaded data (%s) did not match the ETag (%s). Trying again." % (digest, etag)
+			continue
+		else:
+			print "MD5 of downloaded data (%s) did not match the ETag (%s). Giving up after %d tries." % (digest, etag, try_count)
+			exit(1)
+
+
+def execute_bsdiff(oldfile, newfile, patchfile):
+	"""Runs bsdiff with the given arguments. Returns False on failure."""
+	if call([ bsdiff_path, oldfile, newfile, patchfile ]):
+		return False
+	else:
+		return True
+
+
+def generate_patch(oldjar_path, patch_dest):
+	"""
+	Generates a patch from the latest jar to the file at oldjar_path.
+	The patch will be saved to patch_dest.
+	Returns a dict containing information about the version.
+	"""
+
+	vname, ext = os.path.splitext(os.path.basename(oldjar_path))
+
+	# Generate the patch...
+	if not execute_bsdiff(os.path.join(temp_dir_path, "latest.jar"), oldjar_path, patch_dest):
+		print "bsdiff failed to execute. Aborting."
+		exit(1)
+
+
+	# Now check the MD5
+	infile = open(oldjar_path, "rb")
+	m = md5.new(infile.read())
+
+	return { "name": vname, "md5": m.hexdigest() }
+
+
+
+if not os.path.exists(jar_dir_path):
+	print "No jars directory found at " + jar_dir_path
+
+if os.path.exists(temp_dir_path):
+	fsutils.remove_recursive(temp_dir_path)
+os.mkdir(temp_dir_path)
+
+if os.path.exists(output_dir_path):
+	fsutils.remove_recursive(output_dir_path)
+os.mkdir(output_dir_path)
+os.mkdir(os.path.join(output_dir_path, "patches"))
+
+latest_mc_version = determine_latest_version()
+
+print "Downloading latest minecraft.jar..."
+download_version(latest_mc_version, os.path.join(temp_dir_path, "latest.jar"))
+
+
+index = {
+	"mcversion": latest_mc_version,
+	"versions": [],
+	"listversion": 1,
+}
+
+
+# Generate a patch for each file in the jars folder.
+print "Generating patches..."
+for jarfile in os.listdir(jar_dir_path):
+	print "Generating patch for " + jarfile
+	fname, ext = os.path.splitext(os.path.basename(jarfile))
+
+	if ext != ".jar":
+		print "Skipping " + jarfile + " because it doesn't look like a jar file."
+		continue
+	
+	patch_info = generate_patch(os.path.join(jar_dir_path, jarfile), 
+			os.path.join(output_dir_path, "patches", fname + ".patch"))
+	index["versions"].append(patch_info)
+print "Done"
+
+	
+# Dump the index to output.
+index_file = open(os.path.join(output_dir_path, "mcrw-index-v1.json"), "wb")
+index_file.write(json.dumps(index))
+index_file.close()
